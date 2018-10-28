1edd4418cb57b7e51c49a0132bd676619a3ddd03
monangpanggabean <44132115+monangpanggabean@users.noreply.github.com>
2018-10-28 13:08:14 +0700

    Add files via upload
    ditambahkan juga kode C# yang dihasilkan otomatis oleh Umple

---
+++ model_Main.cpp
@@ -0,0 +1,24 @@
+//PLEASE DO NOT EDIT THIS CODE
+//This code was generated using the UMPLE 1.29.1.4295.41a59b8ce modeling language
+#include <SysCallReader.h>
+#include <UsingSystem.h>
+#include <UsingIO.h>
+#include <AnyActions.h>
+#include <AllRunModules.h>
+#include <AllRunExecutions.h>
+#include <Registers.h>
+#include <Function.h>
+#include <Parameters.h>
+#include <Clusterizing.h>
+#include <Classifications.h>
+#include <Localizer.h>
+#include <AllProcesstoBuffers.h>
+#include <MemoryAccess.h>
+#include <HardWareAccess.h>
+#include <ExecutedProcess.h>
+#include <InterfaceforAgentExecManager.h>
+#include <End.h>
+int main(int argc, char *argv[]){
+	
+  return 0;
+}
